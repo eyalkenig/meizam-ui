48ae816b861d0bd092d83a1c9cae62f92d0cd1de
Yaron Sultan <sultanyaron@gmail.com>
2020-03-14 20:49:44 +0200

    refactor(store):moving store definition from App to store index

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 import { ThemeProvider } from '@material-ui/styles';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { rootReducer } from './store';
-import createSagaMiddleware from 'redux-saga';
 import theme from './theme';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import './assets/scss/index.scss';
 import Routes from './Routes';
-
 import { fetchUserInfo } from './store/user/actions';
-import { rootSaga } from './store/sagas';
+import { store, browserHistory } from './store'
 
-const browserHistory = createBrowserHistory();
-const sagaMiddleware = createSagaMiddleware();
-const composeEnhancers = (typeof window !== 'undefined' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-)
-sagaMiddleware.run(rootSaga, browserHistory)
+
 export default class App extends Component {
   componentDidMount() {
     store.dispatch(fetchUserInfo())
@@ -29,13 +16,11 @@ export default class App extends Component {
 
   render() {
     return (
-      <Provider store={store}>
         <ThemeProvider theme={theme}>
           <Router history={browserHistory}>
             <Routes />
           </Router>
         </ThemeProvider>
-      </Provider>
     );
   }
 }
