638b8d77e1f9d71517090812e29c73714afd4ba6
Yaron Sultan <yaron.sultan@herolo.co.il>
2020-03-14 14:11:05 +0200

    Adding styles

--- src/views/CreateGroup/CreateGroup.tsx
+++ src/views/CreateGroup/CreateGroup.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Grid, Theme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    padding: theme.spacing(5)
+  }
+}));
 
 const CreateGroup: FC = () => {
-  return <div>Create group</div>;
+  const classes = useStyles();
+  return <div className={classes.root}>Create group</div>;
 };
 
 export default CreateGroup;
