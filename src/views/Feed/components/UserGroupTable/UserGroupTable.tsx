865faeac9c693e0546347c15343639ecf490dc8c
yael.lotan <yaelotan19@gmail.com>
2020-04-12 13:21:32 +0300

    feat(Feed/UserGroupTable.tsx): mobile carousel

--- src/views/Feed/components/UserGroupTable/UserGroupTable.tsx
+++ src/views/Feed/components/UserGroupTable/UserGroupTable.tsx
@@ -1,11 +1,14 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
     Grid,
     Box,
     Theme,
-    Button
+	Button,
+	Hidden,
+	MobileStepper
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import SwipeableViews from 'react-swipeable-views';
 
 import { UserGroupState, UserState } from '../../../../store/user/types';
 import { PlainFunction } from '../../../../types/interfaces'
@@ -25,7 +28,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
 	viewTable: {
 		marginTop: 20,
-    },
+	},
 }));
 
 interface Props {
@@ -38,45 +41,83 @@ interface Props {
 
 const UserGroupTable: FC<Props> = (props) => {
     const { groups, user, groupNameClicked, showTableClicked, userNameClicked } = { ...props };
-    const classes = useStyles();
+	const classes = useStyles();
+	const [value, setValue] = useState(0);
+
+	interface TableLayoutProps {
+		group: UserGroupState;
+	}
+
+	function TableLayout(props: TableLayoutProps) {
+		const { group } = props;
+
+		return (
+			<Box className={classes.content} display="flex" data-cy='feed-group'>
+				<Box boxShadow={3}>
+					<GroupTable
+						group={group}
+						user={user}	
+						groupNameClicked={groupNameClicked}
+						showTableClicked={showTableClicked}
+						userNameClicked={userNameClicked}
+					/>
+				</Box>
+				<Box textAlign="center">
+					<Button 
+						className={classes.viewTable} 
+						data-cy='show-table-button' 
+						variant="contained" 
+						color="primary" 
+						onClick={() => showTableClicked(group.groupId)}
+					>
+						VIEW TABLE
+					</Button>
+				</Box>
+			</Box>
+		)
+	}
+
+	const handleChangeIndex = (index: number) => {
+		setValue(index);
+	};
 
     return (
-        <Grid container className={classes.gridContainer} spacing={2}>
-			{groups.map((group: UserGroupState) => (
-				<Grid
-					item
-					key={group.groupId}
-					xs={12}
-					sm={6}
-					lg={4}
-					xl={4}
-					data-cy='feed-group'
+		<div>
+			<Hidden mdUp>
+				<SwipeableViews
+					index={value}
+					onChangeIndex={handleChangeIndex}
 				>
-					<Box className={classes.content} display="flex">
-						<Box boxShadow={3}>
-							<GroupTable
-								group={group}
-								user={user}	
-								groupNameClicked={groupNameClicked}
-								showTableClicked={showTableClicked}
-								userNameClicked={userNameClicked}
-							/>
-						</Box>
-						<Box textAlign="center">
-							<Button 
-								className={classes.viewTable} 
-								data-cy='show-table-button' 
-								variant="contained" 
-								color="primary" 
-								onClick={() => showTableClicked(group.groupId)}
-							>
-								VIEW TABLE
-							</Button>
-						</Box>
-					</Box>
+					{groups.map((group: UserGroupState) => 
+						<TableLayout group={group} />
+					)}
+				</SwipeableViews>
+				<MobileStepper
+					variant="dots"
+					steps={groups.length}
+					position="static"
+					activeStep={value}
+					nextButton={<div />}
+					backButton={<div />}
+				/>
+			</Hidden>
+			<Hidden smDown>
+				<Grid container className={classes.gridContainer} spacing={2}>
+					{groups.map((group: UserGroupState) => (
+						<Grid
+							item
+							key={group.groupId}
+							xs={12}
+							sm={6}
+							lg={4}
+							xl={4}
+						>
+							<TableLayout group={group} />
+						</Grid>
+					))}
 				</Grid>
-			))}
-		</Grid>
+			</Hidden>
+		</div>
     )
 }
 
